8f2fd3aa2331e46fb18abab45e439bb6f97c76a6
yoon-m <myoon1098@gmail.com>
2019-05-08 15:52:03 -0400

    remove test code

--- frontend/petsy.jsx
+++ frontend/petsy.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// TEST!!!!!!!!!!!!!!!!!!!!!!
-    import * as ProductApiUtil from './util/product_util';
-// TEST!!!!!!!!!!!!!!!!!!!!!!
-
 document.addEventListener("DOMContentLoaded", () => {
     let store;
     if (window.currentUser) {
@@ -21,12 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         store = configureStore();
     }
-
-    // TEST!!!!!!!!!!!!!!!!!!!
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.fetchProducts = ProductApiUtil.fetchAllProducts;
-    // TEST!!!!!!!!!!!!!!!!!!!
 
     ReactDOM.render(<Root store={ store }/>, document.getElementById('root'))
 });
